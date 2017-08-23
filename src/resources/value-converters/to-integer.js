b346f377236194d1f4ee4bdabf93f287e1f27690
David <david.timovski@gmail.com>
2017-08-24 01:24:15 +0200

    Input field for debt amount changed to a number type

--- src/resources/value-converters/to-integer.js
+++ src/resources/value-converters/to-integer.js
@@ -1,9 +1,5 @@
 export class ToIntegerValueConverter {
     fromView(value) {
-        let converted = Number(value);
-        if (isNaN(converted)) {
-            return 0;
-        }
-        return converted;
+        return Number(value);
     }
 }
